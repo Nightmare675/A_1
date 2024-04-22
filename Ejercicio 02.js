fa67cf471932b96a222d90ce59b6b7dc5ae3cbf3
Nightmare675 <167786654+Nightmare675@users.noreply.github.com>
2024-04-22 09:48:39 -0500

    Ejercicio 02.js

--- Ejercicio 02.js
+++ Ejercicio 02.js
@@ -1,5 +1,3 @@
-/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
-/*2️⃣ EJERCICIO 02 2️⃣*/
 
 function devolverString(string) {
    // Debe retornar un string.
@@ -46,13 +44,3 @@ function obtenerResto(x, y) {
       return x % y;
   }
 }
-
-/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
-module.exports = {
-   devolverString,
-   suma,
-   resta,
-   divide,
-   multiplica,
-   obtenerResto,
-};
