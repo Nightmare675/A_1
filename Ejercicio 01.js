ca5f754f27eedb36873c4132a957653848368435
Nightmare675 <167786654+Nightmare675@users.noreply.github.com>
2024-04-22 09:46:47 -0500

    Add files via upload

---
+++ Ejercicio 01.js
@@ -0,0 +1,36 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*1️⃣ EJERCICIO 01 1️⃣*/
+
+// En los siguientes ejercicios deberás reemplazar el valor null
+// por el correspondiente.
+
+// Crea una variable de tipo string.
+const nuevoString = 'null';
+
+// Crea una variable de tipo number.
+const nuevoNumero = 2712;
+
+// Crea una variable de tipo boolean.
+const nuevoBoolean = false;
+
+// Resuelve el siguiente problema matemático.
+const nuevaResta = 10 - 7 === 3;
+console.log(nuevaResta);
+
+// Resuelve el siguiente problema matemático.
+const nuevaMultiplicacion = 10 * 4 === 40;
+console.log(nuevaMultiplicacion);
+
+// Resuelve el siguiente problema matemático.
+const nuevoModulo = 21 % 5 === 1;
+console.log(nuevoModulo);
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   nuevoString,
+   nuevoNumero,
+   nuevoBoolean,
+   nuevaResta,
+   nuevaMultiplicacion,
+   nuevoModulo,
+};
